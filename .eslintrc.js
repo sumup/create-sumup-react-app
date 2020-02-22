9ac9d8ef8a12774d6a3785dab970bc605dff0da8
Connor Bär <connor.baer@sumup.com>
2020-02-22 10:18:35 -0300

    chore(deps): upgrade to latest Foundry beta

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
-const { node: baseConfig, overwritePresets } = require('@sumup/foundry/eslint');
-
-const customConfig = {
-  overrides: [
-    {
-      files: ['files/**/*.js'],
-      rules: {
-        'notice/notice': 'off',
-        'import/no-unresolved': 'off',
-        'no-unused-vars': 'warn'
-      }
-    }
-  ]
-};
-
-module.exports = overwritePresets(baseConfig, customConfig);
+module.exports = require('@sumup/foundry/eslint')(
+  {
+    language: 'JavaScript',
+    environments: ['Node'],
+    frameworks: [],
+    openSource: true,
+  },
+  {
+    overrides: [
+      {
+        files: ['files/**/*.js'],
+        rules: {
+          'notice/notice': 'off',
+          'import/no-unresolved': 'off',
+          'import/extensions': 'off',
+          'no-undef': 'warn',
+          'no-unused-vars': 'warn',
+        },
+      },
+    ],
+  },
+);
