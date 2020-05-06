40b8901f8f517d64c7beac077195adcdd92c9044
Herbert Henrique <herbert.henrique@sumup.com>
2020-05-06 12:23:05 -0300

    chore(lint): fix configurations using foundry correct params

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,17 @@ module.exports = require('@sumup/foundry/eslint')(
   {
     language: 'JavaScript',
     environments: ['Node', 'Browser'],
+    frameworks: ['React', 'Emotion', 'Jest'],
     openSource: true,
   },
   {
-    env: {
-      node: true,
-    },
     overrides: [
       {
         files: ['files/**/*.js'],
-        env: {
-          browser: true,
-        },
         rules: {
           'notice/notice': 'off',
           'import/no-unresolved': 'off',
           'no-unused-vars': 'warn',
-        },
-      },
-      {
-        files: ['files/**/*.spec.js'],
-        env: {
-          jest: true,
         },
       },
     ],
