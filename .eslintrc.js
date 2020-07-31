a49ea53cdacfe3c44bfa561ea735dd2ac95dfd30
Felix Jung <felix.jung@sumup.com>
2020-07-31 10:08:40 +0200

    Upgrade dependencies and re-init Foundry

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,6 @@
-module.exports = require('@sumup/foundry/eslint')(
-  {
-    language: 'JavaScript',
-    environments: ['Node', 'Browser'],
-    frameworks: ['React', 'Emotion', 'Jest'],
-    openSource: true,
-  },
-  {
-    overrides: [
-      {
-        files: ['files/**/*.js'],
-        rules: {
-          'notice/notice': 'off',
-          'import/no-unresolved': 'off',
-          'no-unused-vars': 'warn',
-        },
-      },
-    ],
-  },
-);
+module.exports = require('@sumup/foundry/eslint')({
+  language: 'JavaScript',
+  environments: ['Node'],
+  frameworks: ['Jest'],
+  openSource: true,
+});
